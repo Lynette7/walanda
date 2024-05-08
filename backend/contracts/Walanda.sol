816ff9ee0980d9e64480add4ea7682ce4aebb3d2
cecilia mulandi <mulandicecilia4@gmail.com>
2024-05-08 12:38:20 +0300

    functions to create expenses

--- backend/contracts/Walanda.sol
+++ backend/contracts/Walanda.sol
@@ -34,4 +34,65 @@ contract ExpenseSplitter {
         uint256 amount
     );
     event ExpenseSettled(uint256 expenseId);
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only contract owner can perform this action");
+        _;
+    }
+
+    modifier onlyExpenseCreator(uint256 _expenseId) {
+        require(
+            msg.sender == expenses[_expenseId].creator,
+            "Only the expense creator can perform this action"
+        );
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function createExpense(
+        string memory _name,
+        string memory _description,
+        uint256 _targetAmount,
+        address[] memory _members
+    ) external {
+        require(_targetAmount > 0, "Target amount must be greater than zero");
+        require(_members.length > 0, "Expense must have at least one member");
+
+        uint256 newExpenseId = expenseId;
+        uint256 contributionPerMember = _targetAmount / _members.length;
+
+        expenses[newExpenseId] = Dutch(
+            newExpenseId,
+            msg.sender,
+            _name,
+            _description,
+            _targetAmount,
+            0,
+            contributionPerMember,
+            false,
+            _members
+        );
+
+        for (uint256 i = 0; i < _members.length; i++) {
+            contributions[newExpenseId][_members[i]] = 0;
+        }
+
+        emit ExpenseCreated(
+            newExpenseId,
+            msg.sender,
+            _name,
+            _description,
+            _targetAmount
+        );
+
+        for (uint256 i = 0; i < _members.length; i++) {
+            emit MemberAdded(newExpenseId, _members[i]);
+        }
+
+        expenseId++;
+    }
+
+
 }
