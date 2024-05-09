2401b07f67001e195b19a88edb25e7f16bd53499
Noela-Murugi <noelatrixie@gmail.com>
2024-05-09 11:42:05 +0300

    update frontend

---
+++ frontend/build/precache-manifest.fa3de51311750180ac3148f1e8f3698f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f256787f035de45ee17d92a4d6d7fd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "25d23d558f84058a0331",
+    "url": "/static/css/main.6799c18e.chunk.css"
+  },
+  {
+    "revision": "1ee80f9f28becd8c5959",
+    "url": "/static/js/2.f4c3e997.chunk.js"
+  },
+  {
+    "revision": "b79550fedf4faedb05b9fa8cc579fa91",
+    "url": "/static/js/2.f4c3e997.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25d23d558f84058a0331",
+    "url": "/static/js/main.ac8f2881.chunk.js"
+  },
+  {
+    "revision": "7074c1300a0c26e41601",
+    "url": "/static/js/runtime-main.9f88373f.js"
+  },
+  {
+    "revision": "0e7144e98e31cc93fe8025e32c2b2d81",
+    "url": "/static/media/blob.0e7144e9.svg"
+  }
+]);
