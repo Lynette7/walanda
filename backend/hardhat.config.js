5386df53c77230cccaa2a6f050c53cd7bed2f507
Lynette Mwangi <69351261+Lynette7@users.noreply.github.com>
2024-05-10 15:28:26 +0300

    Updates

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,21 +1,41 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('dotenv').config();
+require("dotenv").config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
-const { ALCHEMY_API_URL, PRIVATE_KEY } = process.env;
 module.exports = {
-    solidity: "0.8.24",
+    solidity: "0.8.17",
     networks: {
-
-        hardhat: {
-
+        alfajores: {
+            url: "https://alfajores-forno.celo-testnet.org",
+            accounts: [process.env.PRIVATE_KEY],
         },
-        sepolia: {
-            url: ALCHEMY_API_URL,
-            accounts: [`0x${PRIVATE_KEY}`]
-        }
-
-
-
-    }
-
+        celo: {
+            url: "https://forno.celo.org",
+            accounts: [process.env.PRIVATE_KEY],
+        },
+    },
+    etherscan: {
+        apiKey: {
+            alfajores: process.env.CELOSCAN_API_KEY,
+            celo: process.env.CELOSCAN_API_KEY,
+        },
+        customChains: [
+            {
+                network: "alfajores",
+                chainId: 44787,
+                urls: {
+                    apiURL: "https://api-alfajores.celoscan.io/api",
+                    browserURL: "https://alfajores.celoscan.io",
+                },
+            },
+            {
+                network: "celo",
+                chainId: 42220,
+                urls: {
+                    apiURL: "https://api.celoscan.io/api",
+                    browserURL: "https://celoscan.io/",
+                },
+            },
+        ],
+    },
 };
